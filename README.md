b5150eae75f859b33ed1d40af71e95a73f53d4b1
Haroen Viaene <hello@haroen.me>
2016-11-01 15:57:11 +0100

    add dummy index

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This is a caw system for a space shuttle simulation.
 
 See [jwr-shuttle](http://gh.jwronline.be/jwr-shuttle/)!
 
+# Usage
+
+> TODO!!!
+
+1. download the executable via [releases](https://github.com/jwronline/caw/releases)
+2. Start `your-ip:3000` on the touch screen
+3. Connect to `your-ip:3000/admin` as admin to toggle cautions
+
 # License
 
 Apache 2.0
---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
