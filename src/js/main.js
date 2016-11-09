782bbdd0b8244cd771c5b7cb1a387ae54c1e8290
Haroen Viaene <hello@haroen.me>
2016-11-10 00:27:52 +0100

    use kebab-case file names

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ import '../scss/main.scss';
 
 import './dialog.js';
 import socket from './socket.js';
-import showInfo from './showInfo.js';
-import setListeners from './setListeners.js';
+import showInfo from './show-info.js';
+import setListeners from './set-listeners.js';
 
 if (document.body.dataset.role === 'admin') {
   setListeners(socket.emitToggle);
