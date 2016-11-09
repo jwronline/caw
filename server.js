cb1aa88287f79009a7cce0c304e912cc3588a854
Haroen Viaene <hello@haroen.me>
2016-11-09 20:49:44 +0100

    loop over items automatically

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const data = require('./src/solutions.json');
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('main', {role: 'client'});
+  res.render('main', {
+    role: 'client',
+    data: data
+  });
 });
 
 app.get('/admin', (req, res) => {
-  res.render('main', {role: 'admin'});
+  res.render('main', {
+    role: 'admin',
+    data: data
+  });
 });
 
 app.use(express.static(`${__dirname}/dist`));
