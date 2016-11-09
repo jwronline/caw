2008e6ec11b900beeceb70ceaab54a3401c4faf9
Haroen Viaene <hello@haroen.me>
2016-11-10 00:36:08 +0100

    no file extensions in import

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import '../scss/main.scss';
 
-import './dialog.js';
-import socket from './socket.js';
-import showInfo from './show-info.js';
-import setListeners from './set-listeners.js';
+import './dialog';
+import socket from './socket';
+import showInfo from './show-info';
+import setListeners from './set-listeners';
 
 if (document.body.dataset.role === 'admin') {
   setListeners(socket.emitToggle);
